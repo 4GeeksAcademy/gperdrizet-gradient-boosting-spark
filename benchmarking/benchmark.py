6d451b6f91ff352d3aa7321ff3bf47fa09ba2177
George Perdrizet <george@perdrizet.org>
2025-03-16 17:35:09 +0000

    Started setting up hyperparameter optimization benchmark.

---
+++ benchmarking/benchmark.py
@@ -0,0 +1,17 @@
+'''Runs benchmark of hyperparameter optimization rate for gradient boosting models.
+
+Independent variables:
+
+1. Gradient boosting model library
+2. Hyperparameter search space size
+
+Dependent variables:
+
+1. Hyperparameter search rate (parameter sets/minute)
+
+See results.ipynb
+'''
+
+
+if __name__ == '__main__':
+    pass
