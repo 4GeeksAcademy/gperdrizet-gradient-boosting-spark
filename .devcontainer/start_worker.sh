7403bd9b59d7e727e04705b1ab8bba2d5be3ebc8
George Perdrizet <george@perdrizet.org>
2025-03-16 03:12:01 +0000

    Added PySpark environment variables to worker start-up script for location of Python binaries.

--- .devcontainer/start_worker.sh
+++ .devcontainer/start_worker.sh
@@ -6,8 +6,15 @@
 # and opens SSH tunnel to hdeadnode on port 7077. Then starts Spark worker
 # process
 
+# Update the headnode hostname and open SSH tunnel
 mkdir -p ~/.ssh
 git pull
 gh codespace ssh -c `cat .devcontainer/headnode_hostname` --config > ~/.ssh/config
 ssh -vfN -L 7077:localhost:7077 cs.`cat .devcontainer/headnode_hostname`.main
+
+# Make sure PySpark uses the correct python
+export PYSPARK_PYTHON='/usr/local/bin/python'
+export PYSPARK_DRIVER_PYTHON='/usr/local/bin/python'
+
+# Start the worker
 sudo /opt/spark/sbin/start-worker.sh localhost:7077 --webui-port 8080
