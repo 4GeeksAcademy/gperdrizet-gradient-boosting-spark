5f36f21715e380a0f97023dcc614f3d1069cb831
George Perdrizet <george@perdrizet.org>
2025-03-16 03:50:42 +0000

    Removed environment variable exports - don't work here.

--- .devcontainer/start_headnode.sh
+++ .devcontainer/start_headnode.sh
@@ -13,10 +13,6 @@ git add .devcontainer/headnode_hostname
 git commit -m "Updated headnode hostname"
 git push origin main
 
-# Make sure PySpark uses the correct python
-export PYSPARK_PYTHON='/usr/local/bin/python'
-export PYSPARK_DRIVER_PYTHON='/usr/local/bin/python'
-
 # Start a master and worker process
 sudo /opt/spark/sbin/start-master.sh --port 7077 --host localhost --webui-port 8080
 sudo /opt/spark/sbin/start-worker.sh localhost:7077 --webui-port 8081
--- .devcontainer/start_worker.sh
+++ .devcontainer/start_worker.sh
@@ -12,9 +12,5 @@ git pull
 gh codespace ssh -c `cat .devcontainer/headnode_hostname` --config > ~/.ssh/config
 ssh -vfN -L 7077:localhost:7077 cs.`cat .devcontainer/headnode_hostname`.main
 
-# Make sure PySpark uses the correct python
-export PYSPARK_PYTHON='/usr/local/bin/python'
-export PYSPARK_DRIVER_PYTHON='/usr/local/bin/python'
-
 # Start the worker
 sudo /opt/spark/sbin/start-worker.sh localhost:7077 --webui-port 8080
